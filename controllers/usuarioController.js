4476345b57b73c9e22ddb0c3d33648e723d6507d
Julian Martinez <julian.fmartinez@gmail.com>
2022-05-20 00:20:31 -0300

    add: Se agrega express-validator y bcrypt para hashear passwords

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,13 +1,35 @@
-import Usuario from "../models/Usuario.js";
+import Usuario from '../models/Usuario.js';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
 
 const nuevoUsuario = async (req, res) => {
+  //Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+  const { email, password } = req.body;
 
-    const usuario = await new Usuario(req.body);
+  const usuarioExiste = await Usuario.findOne({ email: email });
 
-    res.json({msg: "Usuario creado correctamente"});
-    usuario.save()
-}
+  if (usuarioExiste) {
+    res.status(400).json({ msg: 'El usuario ya existe' });
+    return;
+  }
 
-export {
-    nuevoUsuario
-}
+  //Crear el nuevo usuario
+  const usuario = await new Usuario(req.body);
+  //Encriptar el password
+  const salt = await bcrypt.genSalt(10);
+  usuario.password = await bcrypt.hash(password, salt);
+
+  //Guardar el usuario
+  try {
+    await usuario.save();
+    res.status(200).json({msg:'Usuario creado correctamente'});
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { nuevoUsuario };
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { check } from 'express-validator'
 const router = express.Router();
-
 import { nuevoUsuario } from "../controllers/usuarioController.js";
 
-router.post("/", nuevoUsuario);
+router.post("/", [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'Agrega un email válido').isEmail(),
+    check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6 })
+    ],
+    nuevoUsuario
+);
 
 export default router;
